3cebcccb239e741e7f8bc1959b3b3da1d79a87e3
dkokonkwo <d.okonkwo@alustudent.com>
2023-02-21 04:55:11 -0800

    1-calculation.py script

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3                                                                                 
+# Functions of basic math operations                                                                          
+
+def add(a, b) -> int:
+    return (a + b)
+
+def sub(a, b) -> int:
+    return (a - b)
+
+def mul(a, b) -> int:
+    return (a * b)
+
+def div(a, b) -> int:
+    return int(a / b)
