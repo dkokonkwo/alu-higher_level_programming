44eb9a94fe62371ba99d0ecd3c8c4cb783f2b052
dkokonkwo <d.okonkwo@alustudent.com>
2023-02-22 05:31:10 -0800

    100-my_calculator.py script

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,26 +1,22 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 if __name__ == '__main__':
-    import  sys
-    from calculator_1 import  add, sub, mul, div
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-    operators = ['+', '-', '*', '/']
-    if (len(sys.argv) - 1) != 3:
+    list_args = sys.argv[1:]
+    if len(list_args) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[1] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    
-    list_args = sys.argv[1:]
     a = int(list_args[0])
-    b = int(list_args[-1])
-    for arg in list_args:
-        if list_args[1] == '+':
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        if list_args[1] == '':
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        if list_args[1] == '*':
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        if list_args[1] == '/':
-            print("{} / {} = {}".format(a, b, div(a, b)))
+    b = int(list_args[2])
+    if list_args[1] == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif list_args[1] == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif list_args[1] == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif list_args[1] == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
