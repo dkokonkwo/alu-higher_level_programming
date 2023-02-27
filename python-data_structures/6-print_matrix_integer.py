41ee8839a8e436eade760df7db4614d843503953
dkokonkwo <d.okonkwo@alustudent.com>
2023-02-27 07:55:39 -0800

    6-print_matrix_integer.py script 2

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,9 +4,9 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for col in row:
                 if col == row[0]:
-                    print("{:d}".format(col), end=" ")
+                    print("{:d}".format(col), end="")
                 else:
-                    print("{:d}".format(col), end=" ")
+                    print(" {:d}".format(col), end="")
             print()
     else:
         print()
