4983b1164780a6dbb15baf04a196ed75f47725b2
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-06 04:43:52 -0800

    0-safe_print_list.py script 2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for index, value in enumerate(my_list):
-            if index < x:
-                print(value, end='')
-        print()
-        if x <= (index + 1):
-            return x
-        else:
-            return index + 1
-    except:
-        print("There was an error somewhere!")
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except:
+            continue
+    print()
+    return count
