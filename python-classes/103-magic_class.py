070c9c2290b9a9cff301e4d16ef11978e276e2e0
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-15 08:00:45 -0700

    103-magic_class.py script 1

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """create a class MagicClass"""
 
 
 import math
 
 class MagicClass:
+    """defines initiation, area and circumfrence methods"""
     def __init__(self, radius=0) -> None:
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
