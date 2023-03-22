d3dd4037a62a93f86c6c9ba350e22e9ff7a6810f
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-22 07:04:11 -0700

    3-is_kind_of_class.py script

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function to check in an object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if obj is an instance of a_class"""
+    return isinstance(obj, a_class)
