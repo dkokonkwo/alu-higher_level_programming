170e944f077e09901b90fc88292c0cb07196c45b
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-22 08:39:44 -0700

    10-square.py script

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines class Square that inherits from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        super().__init__(size, size)
+
+    def area(self):
+        """calulates and returns area of square"""
+        return self.__size * self.__size
