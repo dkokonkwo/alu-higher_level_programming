e8f72a8ffac3f585a273d87dc7462affdee41350
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-22 07:29:55 -0700

    6-base_geometry.py script

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creates class BaseGeometry and defines its methods"""
+
+
+class BaseGeometry():
+    """methods of BaseGeometry"""
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
