c415d2d3fd51f3b728168ae18487abe471e9fa4a
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-22 07:15:12 -0700

    rectified error for Q4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """returns True if obj is an instance of a class inherited from a specified a_class"""
+    """returns True if obj is an instance of a specified a_class"""
     return isinstance(obj, a_class) and type(obj) is not a_class
