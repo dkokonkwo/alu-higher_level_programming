50124a66d7fb526fe47b5d9703fc723be31675b3
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-23 08:14:26 -0700

    5-save_to_json_file.py script 2

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,7 +7,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """converts object to json and writes it to a file"""
-    y = json.dumps(my_obj)
+    y = json.dumps(my_obj, ensure_ascii=False)
     with open(filename, "w") as f:
         f.write(y)
         f.close
