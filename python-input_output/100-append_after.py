d834ff9b0452eff4e854e1642f8b0207891ea314
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-24 06:22:47 -0700

    100-append_after.py script 2

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -4,9 +4,14 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """reads the file and replaces the string"""
+    new_txt = ''
     with open(filename, "r") as f:
-        content = f.read()
-        content = content.replace(search_string, new_string)
+        for line in f:
+            line = line.strip()
+            new_line = line.replace(search_string, new_string)
+            new_txt = new_txt + new_line + "\n"
+
+    f.close()
     with open(filename, "w") as f:
-        f.write(content)
+        f.write(new_txt)
         f.close()
