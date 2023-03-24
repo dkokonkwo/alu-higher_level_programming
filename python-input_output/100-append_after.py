ddefe3656b4c6c3004733300553e209d626a37a3
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-24 06:24:19 -0700

    100-append_after.py script 3

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Replace an old string with a new one"""
+"""defines function to append text after each line with given string"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """reads the file and replaces the string"""
-    new_txt = ''
-    with open(filename, "r") as f:
-        for line in f:
-            line = line.strip()
-            new_line = line.replace(search_string, new_string)
-            new_txt = new_txt + new_line + "\n"
-
-    f.close()
-    with open(filename, "w") as f:
-        f.write(new_txt)
-        f.close()
+    """appends text after each line with given string"""
+    txt = ""
+    with open(filename, 'r') as f:
+        f_line = f.readline()
+        while f_line != "":
+            txt += f_line
+            if search_string in f_line:
+                txt += new_string
+            f_line = f.readline()
+    with open(filename, 'w') as f:
+        f.write(txt)
