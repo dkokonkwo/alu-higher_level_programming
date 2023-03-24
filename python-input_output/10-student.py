63e79b91d3c15c7b3b7138d6a2378859d6597792
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-24 03:55:52 -0700

    10-student.py script

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""creates student class with public instance attributes"""
+
+
+class Student:
+    """initializes instance attributes"""
+    def __init__(self, first_name, last_name, age) -> None:
+        """public instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None) -> dict:
+        """returns attr if in self.__dict else returns self.__dict__"""
+        if type(attrs) is list:
+            n_dict = {}
+            for key in self.__dict__:
+                for i in attrs:
+                    if i == key:
+                        n_dict[i] = self.__dict__[i]
+            return n_dict
+        else:
+            return self.__dict__
