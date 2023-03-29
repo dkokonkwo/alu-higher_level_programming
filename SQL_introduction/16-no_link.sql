8b69d2dca4d1f908aaf5a017f109da662a7a050d
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-29 05:51:04 -0700

    16-no_link.sql script 2

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- show records with name and score
-SELECT name, score FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
