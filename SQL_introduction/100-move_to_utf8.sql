07cdf5df142d28f7729b67f78ea37adfb8362dd6
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-29 12:15:42 -0700

    100-move_to_utf8.sql script7

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- converts database to UTF-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name BINARY;
 ALTER TABLE first_table TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
