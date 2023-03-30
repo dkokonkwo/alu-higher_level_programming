1619304440e79425d7c74fae6f57d67827cef54c
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-30 11:18:42 -0700

    8-cities_of_california_subquery.sql script4

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- lists cities of Carlifornia
 SELECT id, name FROM cities
-WHERE states_id = (SELECT id FROM states WHERE name = 'Carlifornia')
+WHERE state_id = (SELECT id FROM states WHERE name = 'Carlifornia')
 ORDER BY id ASC;
