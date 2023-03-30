8824dddbcc186747fcec803caef122e45176db23
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-30 10:07:16 -0700

    0-privileges.sql script2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- show privileges for user on server
-SHOW GRANTS FOR 'user_0d_1'@'local_host';
-SHOW GRANTS FOR 'user_0d_2'@'local_host';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
