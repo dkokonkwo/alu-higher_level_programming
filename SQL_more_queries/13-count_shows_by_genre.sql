63a991867a4421ec5cd9292d1df9034b531cdb5e
dkokonkwo <d.okonkwo@alustudent.com>
2023-03-31 03:15:06 -0700

    13-count_shows_by_genre.sql script5

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT g.name AS genre, COUNT(t.show_id) AS number_of_shows
        FROM tv_genres AS g
        LEFT JOIN tv_show_genres AS t
        ON g.id = t.genre_id
-       WHERE number_of_shows > 0
+       WHERE t.show_id IS NOT NULL
        GROUP BY g.id
        ORDER BY number_of_shows DESC;
