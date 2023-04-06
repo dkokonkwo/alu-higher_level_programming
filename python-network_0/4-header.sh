53de62c27a495588e6e5627ba02eef2778d747ba
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-06 12:08:02 +0000

    4-header.sh script4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends GET request to given URL, with key-value, and displays body of response
-curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sX GET -H "X-HolbertonSchool-User-Id:98" "$1"
