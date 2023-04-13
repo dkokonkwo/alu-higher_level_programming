fc66a8d3a6524a60f4ec7f47e84b811319ccdb99
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-13 05:50:07 -0700

    0-add_integer.py script3

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@
 
 def add_integer(a, b=98):
     """returns the sum of a and b"""
-    if type(a) not in [int,float]:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
