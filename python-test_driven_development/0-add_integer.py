29d119ecbdef0e72fd4381948d76aa403fc6741b
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-13 05:42:06 -0700

    add_integer scripts

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines an addition function"""
+
+
+def add_integer(a, b=98):
+    """returns the sum of a and b"""
+    if type(a) not in [int,float]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
