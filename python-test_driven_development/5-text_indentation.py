58bc9d6e8eaf71014ed3261d03dedfc43dfe9d6b
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-13 07:44:41 -0700

    text_indentation scripts

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines a function that prints text with new lines"""
+
+
+def text_indentation(text):
+    """add 2 new lines when char is . or :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    new_string = ""
+    for character in text:
+        if character == "." or character == ":":
+            new_string += character + "\n\n"
+        else:
+            new_string += character
+    print(new_string)
