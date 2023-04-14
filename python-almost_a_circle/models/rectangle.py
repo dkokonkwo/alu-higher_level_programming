6cc1eb6fdcc9c87d729ae5b7fe6ef675fd582eeb
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-14 07:21:14 -0700

    rectangle.py script4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,8 @@ from models.base import Base
 
 
 class Rectangle(Base):
+     """class for Rectangle instances with
+    private instance attributes width, height, x, and y"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super.__init__(id)
         self.width = width
