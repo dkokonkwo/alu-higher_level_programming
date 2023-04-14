a114d7c807b5db2a6a1b9a9b027022dd80234cb6
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-14 08:04:27 -0700

    added update function

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""defines Rectangle class that inherits from Base"""
+0;276;0c"""defines Rectangle class that inherits from Base"""
 
 
 from models.base import Base
@@ -94,3 +94,31 @@ class Rectangle(Base):
             str(self.id), str(self.x), str(self.y),
             str(self.width), str(self.height))
         return (str_rep)
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute of Rectangle"""
+        attr_list = ["id", "width", "height", "x", "y"]
+        if args and len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                if i == 1:
+                    self.width = args[i]
+                if i == 2:
+                    self.height = args[i]
+                if i == 3:
+                    self.x = args[i]
+                if i == 4:
+                    self.y = args[i]
+        else:
+            for kw in kwargs:
+                if kw == "id":
+                    self.id = (kwargs[kw])
+                if kw == "width":
+                    self.width = (kwargs[kw])
+                if kw == "height":
+                    self.height = (kwargs[kw])
+                if kw == "x":
+                    self.x = (kwargs[kw])
+                if kw == "y":
+                    self.y = (kwargs[kw])
