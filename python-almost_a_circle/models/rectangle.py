0d081723ce7522e033061de36fd884493c60259a
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-14 07:18:10 -0700

    rectangle.py script3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,9 @@
 """class inherits from Base class"""
 
 
+from models.base import Base
+
+
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super.__init__(id)
