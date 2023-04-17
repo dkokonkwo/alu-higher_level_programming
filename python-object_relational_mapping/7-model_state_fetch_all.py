d81b3ee4aeba676b84c6a2c2725bd3f8d52174a0
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-17 22:45:56 +0200

    7-model_state_fetch_all.py more

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,6 +3,7 @@
 lists all State objects from the database in ascending order
 using SQLAlchemy and importing State and Base from model_state
 """
+
 from sys import argv
 from model_state import Base, State
 
@@ -10,6 +11,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    """Access to the database and get the states"""
+
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             argv[1], argv[2], argv[3])
         engine = create_engine(db_uri)
