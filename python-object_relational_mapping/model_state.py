8559d14bef7e14b2fc4c00e909cf568744d66b3f
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-17 22:22:30 +0200

    model_state.py script

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+defines State class that inherits from Base class
+and links to sql
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+clas State(Base):
+    """define State that links to MySQL table"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
