8b48e19ddfa0793daeddb8ef4011ee1359e0dcd7
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-17 12:13:31 -0700

    fixed errors

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,7 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[[3]]
-    )
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = conn.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
