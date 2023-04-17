a5ff856f75b8ea2d381701450536460c3dba2515
dkokonkwo <d.okonkwo@alustudent.com>
2023-04-17 12:53:36 -0700

    4-cities_by_state.py script

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT c.id, c.name, s.name FROM cities as c \
-                INNER JOIN states as s ON c.states_id = s.id \
+                INNER JOIN states as s ON c.state_id = s.id \
                 ORDER BY c.id")
     query_rows = cur.fetchall()
     for row in query_rows:
