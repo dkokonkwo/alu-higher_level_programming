7f5f2fdaefbeecb56e4634084518e3e4651509d2
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-10 15:06:03 -0700

    indentation

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,12 +5,12 @@ const num = Number(myArgs[0]);
 if (isNaN(num)) {
   console.log('Missing size');
 } else {
-    let i = 0;
-    string = '';
-    for (i = 0; i < num; i++) {
-      string += myVar;
-    }
-    for (i = 0; i < num; i++) {
-      console.log(string);
-    }
+  let i = 0;
+  string = '';
+  for (i = 0; i < num; i++) {
+    string += myVar;
+  }
+  for (i = 0; i < num; i++) {
+    console.log(string);
+  }
 }
