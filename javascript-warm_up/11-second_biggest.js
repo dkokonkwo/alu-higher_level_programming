fb8ca851357f2412df8d955882c2d82bb06034db
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-12 04:35:42 -0700

    camel case

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
 if (myArgs.length === 0 || myArgs.length === 1) {
-  console.log(0)
+  console.log(0);
 } else {
   let i = 0;
   let max = 0;
-  let max_2 = 0;
+  let maxt = 0;
   if (Number(myArgs[0]) > Number(myArgs[1])) {
     max = myArgs[0];
-    max_2 = myArgs[1];
+    maxt = myArgs[1];
   } else {
     max = myArgs[1];
-    max_2 = myArgs[0];
+    maxt = myArgs[0];
   }
   for (i = 2; i < myArgs.length; i++) {
     if (Number(myArgs[i]) > max) {
-      max_2 = max;
+      maxt = max;
       max = myArgs[i];
-    } else if (Number(myArgs[i]) < max && Number(myArgs[i]) > max_2) {
-      max_2 = myArgs[i];
+    } else if (Number(myArgs[i]) < max && Number(myArgs[i]) > maxt) {
+      maxt = myArgs[i];
     }
   }
-  console.log(max_2)
+  console.log(maxt)
 }
