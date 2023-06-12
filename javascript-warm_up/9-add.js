a1dab374f4720d2eb899d24e56de397202a00334
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-12 03:38:39 -0700

    added spaces

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const myArgs = process.argv.slice(2);
 const num1 = Number(myArgs[0]);
 const num2 = Number(myArgs[1]);
-function add(a, b) {
-  return(a + b);
+function add (a, b) {
+  return (a + b);
 }
 console.log(add(num1, num2));
