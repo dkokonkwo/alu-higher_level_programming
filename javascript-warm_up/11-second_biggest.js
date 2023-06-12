6e69eb1e6751f1bf1fc894d322c764f79069c3ee
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-12 04:25:44 -0700

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+if (myArgs.length == 0 || myArgs.length == 1) {
+  console.log(0)
+} else {
+  let i = 0;
+  let max = 0;
+  let max_2 = 0;
+  if (Number(myArgs[0]) > Number(myArgs[1])) {
+    max = myArgs[0];
+    max_2 = myArgs[1];
+  } else {
+    max = myArg[1];
+    max_2 = myArgs[0];
+  }
+  for (i = 2; i < myArgs.length; i++) {
+    if (Number(myArgs[i]) > max) {
+      max_2 = max;
+      max = myArgs[i];
+    } else if (Number(myArgs[i]) < max && Number(myArgs[i]) > max_2) {
+      max_2 = myArgs[i];
+    }
+  }
+  console.log(max_2)
+}
