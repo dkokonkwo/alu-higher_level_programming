012009456867e09abbba5c6e0fb61ad8937af7f0
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-13 14:09:27 -0700

    removed test

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -9,7 +9,3 @@ function nbOccurences (list, searchElement) {
   }
   return count;
 }
-
-console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
-console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(['S', 12, 'c', 'S', 'School', 8], 'S'));
