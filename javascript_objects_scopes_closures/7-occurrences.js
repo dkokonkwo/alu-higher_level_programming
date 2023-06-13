4cb06477e492791c70ae959073fb7172f3687a32
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-13 14:02:43 -0700

    executable 7

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -12,4 +12,4 @@ function nbOccurences (list, searchElement) {
 
 console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
 console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
+console.log(nbOccurences(['S', 12, 'c', 'S', 'School', 8], 'S'));
