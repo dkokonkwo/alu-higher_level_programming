d66a0e50be31265f4f180a55b501d17aea9586e0
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-14 15:06:11 +0200

    good code

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,15 +1,4 @@
 #!/usr/bin/node
-const testList = require('./100-data').list;
-const mp = new Map();
-const newList = [];
-// split list into key and value
-let i = 0;
-for (i = 0; i < testList.length; i++) {
-  mp.set(testList[i], i);
-}
-// multipy and add the new array
-for (const entry of mp) {
-  newList.push(entry[0] * entry[1]);
-}
-console.log(testList);
-console.log(newList);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
