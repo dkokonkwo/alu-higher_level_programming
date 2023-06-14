b0d3856fbc95f4b561b7da85103a56eab10ebad4
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-14 05:56:54 -0700

    should be good

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const testList = require('./100-data').list;
-let mp = new Map();
-let newList = [];
+const mp = new Map();
+const newList = [];
 // split list into key and value
 let i = 0;
 for (i = 0; i < testList.length; i++) {
   mp.set(testList[i], i);
 }
 // multipy and add the new array
-for (let entry of mp) {
+for (const entry of mp) {
   newList.push(entry[0] * entry[1]);
 }
 console.log(testList);
