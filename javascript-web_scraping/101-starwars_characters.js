77d4d453ed4c5215e41833c6d344e595d16f9582
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-15 15:35:29 +0200

    test this

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,29 +1,20 @@
 #!/usr/bin/node
-// script that prints all characters of a Star Wars movie
 const request = require('request');
-const myArgs = process.argv.slice(2);
-const URLstring = 'https://swapi-api.alx-tools.com/api/films/' + myArgs[0];
-// recursive function
-function characterFunc (character, i = 0, stop) {
-  if (i === stop) {
-    return;
-  }
-  const URLcharacter = character[i];
-  request(URLcharacter, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(JSON.parse(body).name);
-    }
-  });
-  i++;
-  characterFunc(character, i, stop);
-}
-request(URLstring, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const character = JSON.parse(body).characters;
-    characterFunc(character, 0, character.length);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
