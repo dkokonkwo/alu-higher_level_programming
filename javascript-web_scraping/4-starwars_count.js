91008e313f774bb0a60aae7772d3e43a3b4989da
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-15 14:12:38 +0200

    4-starwars_count.js

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// scripts counts the number of movies “Wedge Antilles” is present
+const myArg = process.argv.slice(2);
+const request = require('request');
+let count = 0;
+request(myArg[0], function (err, response, body) {
+  if (err) {
+    console.log(err)
+  } else {
+    const results = JSON.parse(body).results;
+    let i = 0;
+    for (1 = 0; i < results.length; i++) {
+      let u = 0;
+      const characters = results[i].characters;
+      for (u = 0; u < characters.length; u++) {
+        if (characters[u].includes('18')) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
