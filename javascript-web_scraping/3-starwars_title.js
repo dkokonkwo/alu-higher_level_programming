44d223b83a3e96189d44f3ac242e09fffb5a58e6
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-15 13:57:36 +0200

    test this

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const myArg = process.argv.slice(2);
-const urlString = 'https://swapi-api.alx-tools.com/api/films/:id' + myArg[0];
+const urlString = 'https://swapi-api.alx-tools.com/api/films/' + myArg[0];
 request(urlString, function (err, response, body) {
   if (err) {
     console.log(err);
