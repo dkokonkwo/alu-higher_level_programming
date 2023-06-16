50d32407fc39407dd22999bd2cd411cc2374bb80
dkokonkwo <d.okonkwo@alustudent.com>
2023-06-16 15:38:48 +0200

    100-script.js

---
+++ javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// updates the text color of header
+// usinng document.querySelector
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style = 'color: red;';
+});
